f5545b9b90e3417d45355aa8fc0104be078e42e1
A 027 <siddhardhach3791@gmail.com>
2024-11-18 22:04:32 +0530

    done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,22 +8,22 @@ function Home() {
   
   const [isBottomScreenVisible, setBottomScreenVisible] = useState(false);
   const [models] = useState([
-    { name: "armchair", image: "/images/armchair.png", glb: "/models/armchair.glb", target: 0.01},
-    { name: "lounger", image: "/images/lounger.png", glb: "/models/lounger.glb", target:0.01 },
-    { name: "coffeetable", image: "/images/coffeetable.png", glb: "/models/coffeetable.glb", target: 0.005 },
-    { name: "nesttable", image: "/images/nesttable.png", glb: "/models/nesttable.glb", target: 0.01 },
-    { name: "armchairv", image: "/images/armchairv.png", glb: "/models/armchairv.glb", target: 0.01 },
-    { name: "fliptable", image: "/images/fliptable.png", glb: "/models/filptable.glb", target: 0.01 },
-    { name: "armchairk", image: "/images/armchairk.png", glb: "/models/armchairk.glb", target:4 },
-    { name: "armchairp", image: "/images/armchairp.png", glb: "/models/armchairp.glb", target:1.6 },
-    { name: "carpet", image: "/images/carpet.png", glb: "/models/carpet.glb", target: 3 },
-    { name: "coffee set", image: "/images/coffeeset.png", glb: "/models/coffeeset.glb", target: 1.2},
-    { name: "dinning table", image: "/images/dintable.png", glb: "/models/dintable.glb", target: 3 },
-    { name: "fcouch", image: "/images/fcouch.png", glb: "/models/fcouch.glb", target:2.2 },
-    { name: "lamp", image: "/images/lamp.png", glb: "/models/lamp.glb", target: 5 },
-    { name: "rtable", image: "/images/rtable.png", glb: "/models/rtable.glb", target: 5 },
-    { name: "side board", image: "/images/sideboard.png", glb: "/models/sideboard.glb", target:3 },
-    { name: "tv stand", image: "/images/tvstand.png", glb: "/models/tvstand.glb", target: 3 }
+    { name: "Arm Chair", image: "/images/armchairk.png", glb: "/models/armchairk.glb", target:4 },
+    { name: "Lounger", image: "/images/lounger.png", glb: "/models/lounger.glb", target:0.01 },
+    { name: "Arm Chair Yellow", image: "/images/armchair.png", glb: "/models/armchair.glb", target: 0.01},
+    { name: "Nested Table", image: "/images/nesttable.png", glb: "/models/nesttable.glb", target: 0.01 },
+    { name: "Flip Table", image: "/images/fliptable.png", glb: "/models/filptable.glb", target: 0.01 },
+    { name: "Arm Chair Large", image: "/images/armchairp.png", glb: "/models/armchairp.glb", target:1.6 },
+    { name: "Dinning Table", image: "/images/dintable.png", glb: "/models/dintable.glb", target: 3 },
+    { name: "Carpet", image: "/images/carpet.png", glb: "/models/carpet.glb", target: 3 },
+    { name: "Side Board", image: "/images/sideboard.png", glb: "/models/sideboard.glb", target:3 },
+    { name: "TV Stand", image: "/images/tvstand.png", glb: "/models/tvstand.glb", target: 3 },
+    { name: "Coffee Set", image: "/images/coffeeset.png", glb: "/models/coffeeset.glb", target: 1.2},
+    { name: "Coffee Table", image: "/images/coffeetable.png", glb: "/models/coffeetable.glb", target: 0.005 },
+    { name: "Arm Chair violet", image: "/images/armchairv.png", glb: "/models/armchairv.glb", target: 0.01 },
+    { name: "Couch", image: "/images/fcouch.png", glb: "/models/fcouch.glb", target:2.2 },
+    { name: "Lamp", image: "/images/lamp.png", glb: "/models/lamp.glb", target: 5 },
+    { name: "Round Table", image: "/images/rtable.png", glb: "/models/rtable.glb", target: 5 },
   ]);
   const [selectedModel, setSelectedModel] = useState(null);
   const navigate = useNavigate();  // Initialize navigate hook
