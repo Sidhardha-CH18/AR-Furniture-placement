e398d1290fd7f1b8d9ff61aa97f9b59b8697e6d3
A 027 <siddhardhach3791@gmail.com>
2024-12-20 08:28:27 +0530

    f

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import BottomScreen from "../components/BottomScreen";
+import Header from "../components/Header";
 
 function Home() {
   const [isBottomScreenVisible, setBottomScreenVisible] = useState(false);
